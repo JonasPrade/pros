67d7b59cc89af1a1c1a61517f8677a5e81d4ae78
JonasPrade <j.prade@campus.tu-berlin.de>
2024-01-07 14:23:12 +0100

    cleaned manage.py

--- manage.py
+++ manage.py
@@ -3,16 +3,5 @@ from prosd import app
 
 cli = FlaskGroup(app)
 
-# TODO: Add cli command for creating an user
-
-
-"""
-@cli.command("create_db")
-def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commmit()
-"""
-
 if __name__ == '__main__':
     cli()
